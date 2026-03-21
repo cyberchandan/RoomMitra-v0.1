739e4537350db1c07153931a873d790f094916b9
Chandan Kumar <chandan36024@gmail.com>
2026-03-21 17:07:16 +0530

    base url changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend base URL
+  // baseURL: 'http://localhost:5000/api', // Backend base URL
+  baseURL: 'https://roomserver.vercel.app/api', // Backend base URL
+
 });
 
 // Interceptor to add token to requests
